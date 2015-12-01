e3855f8f161c40dc0f8423ce35236128d3d1709e
mayormcmatt <matthew.is.riding@gmail.com>
2015-11-30 23:46:29 -0800

    Fixed pageload JS issue; next up is scrolling nav click

--- hioeb/public/js/main.js
+++ hioeb/public/js/main.js
@@ -1,6 +1,15 @@
 // On page load, resize window to viewport height
-$(document).ready(function(){
+function resizeTopBanner() {
     var viewportHeight = $(window).height();
-
     $('#titleBanner').height(viewportHeight - 200);
-})
+}
+
+$('#servicesLink').click(function() {
+    $('html, body').animate({
+        scrollTop: $('#servicesRow').offset().top}, 'fast'
+    );
+});
+
+$(document).ready(function() {
+    resizeTopBanner();
+});
